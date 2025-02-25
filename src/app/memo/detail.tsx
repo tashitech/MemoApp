e622fd55d3448686f2e404eef26efb3079216900
tashitech <ey1223tk@gmail.com>
2025-02-25 23:36:02 +0900

    add detail.tsx and modify structure and create components

---
+++ src/app/memo/detail.tsx
@@ -0,0 +1,62 @@
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import Header from '../../components/Header';
+import CircleButton from '../../components/CircleButton';
+
+const Detail = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト１</Text>
+        <Text style={styles.memoDate}>2025年2月1日 12:00</Text>
+      </View>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoBodyText}>
+          今日の買い物のリスト
+          高タンパク質・低カロリーな夕食を作りたいので、鶏胸肉・ブロッコリーは必須です。
+          忘れないように！
+        </Text>
+      </ScrollView>
+      <CircleButton style={{top: 160, bottom: 'auto'}}>
+        <Feather name="check" size={40} />
+      </CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  memoHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19
+  },
+  memoTitle: {
+    color: '#fff',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold'
+  },
+  memoDate: {
+    color: '#fff',
+    fontSize: 12,
+    lineHeight: 16
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27
+  },
+  memoBodyText: {
+    color: '#000',
+    fontSize: 16,
+    lineHeight: 24
+  }
+})
+
+export default Detail;
